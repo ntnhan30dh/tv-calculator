03ddc4adee876f53e2493243b6206afa0f91861f
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-02-23 17:32:03 +0100

    Ad Fatigue Calculator

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
               <div className="leftWrapper">
                 <div className="top">
                   <img src={logo} alt="logo" />
-                 TVC Media Pressure Optimiser
+                 Ad Fatigue Calculator
                 </div>
                 <div className="bottom">
                   <Login setLoginState={setLoginState} />
@@ -43,7 +43,7 @@ const App = () => {
               <div className="leftWrapper">
                 <div className="top">
                   <img src={logo} alt="logo" />
-                 TVC Media Pressure Optimiser
+                 Ad Fatigue Calculator
                 </div>
                 <div className="bottom">
                   <Logout setLoginState={setLoginState} />
